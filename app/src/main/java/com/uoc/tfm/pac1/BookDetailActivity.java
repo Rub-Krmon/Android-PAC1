6f3fd47f9010c79266c9dde645de2a5593988eee
Ruben Carmona <rub.krmon@gmail.com>
2016-09-30 07:13:03 +0200

    Added Fragments .. or something like that

---
+++ app/src/main/java/com/uoc/tfm/pac1/BookDetailActivity.java
@@ -0,0 +1,13 @@
+package com.uoc.tfm.pac1;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class BookDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.id.book_detail);
+    }
+}
